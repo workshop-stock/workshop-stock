f6989f864baa787797301941915c912e4ec18eb2
workshop-stock <jp.mahon@hotmail.com>
2026-05-27 14:45:57 +0100

    Add files via upload

---
+++ config for firebase data base for workshop.js
@@ -0,0 +1,12 @@
+// Workshop Stock Control — Firebase Configuration
+// This file contains the database credentials.
+// Keep this file private — do not share publicly.
+window.__wsc_cfg = {
+  "apiKey": "AIzaSyA419gjuu0wF591zibiikQ8fqOBezedZsk",
+  "authDomain": "workshop-stock-4689a.firebaseapp.com",
+  "projectId": "workshop-stock-4689a",
+  "storageBucket": "workshop-stock-4689a.firebasestorage.app",
+  "messagingSenderId": "198436659283",
+  "appId": "1:198436659283:web:470d69b5fd6037c835e6a6",
+  "databaseURL": "https://workshop-stock-4689a-default-rtdb.europe-west1.firebasedatabase.app"
+};
